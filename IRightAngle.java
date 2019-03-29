4830f0d8e242b6ce8cd08b05292bca4706c6aacc
NadeemAhmad007 <44241167+NadeemAhmad007@users.noreply.github.com>
2019-03-29 21:18:22 +0100

    Update IRightAngle.java

--- IRightAngle.java
+++ IRightAngle.java
@@ -3,3 +3,5 @@ public interface IRightAngle {
 	public double pythagoras();
 
 }
+// Either this interface has no application in this task or i could not understand.Please make a commit in main.java class so that i can
+//understand the purpose of this interface used in this task.Thank you
